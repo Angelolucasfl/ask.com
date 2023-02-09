ca228415448b5b50beed91bf654e38122442b319
Angelo Lucas <Angelolucasfl@gmail.com>
2023-02-08 21:39:19 -0300

    first commit

--- users/views.py
+++ users/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 
-def index(request):
-    return render(request, 'users/login.html')
+
+def logar(request):
+    if request.method == 'GET':
+        return render(request, 'users/login.html')
+    
+    elif request.method == 'POST':
+        nome_usuario = request.POST.get('nome_usuario')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=nome_usuario, password=senha)
+
+        if user is not None:
+            login(request, user)
+            return redirect('admin/')
+        else:
+            return render(request, 'users/login.html')
